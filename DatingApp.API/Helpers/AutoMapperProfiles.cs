3d24c6af773b2de2be19e5774da5d1ca2c972da0
ahmedmokhtar97 <ahmedmokhtar46@gmail.com>
2019-10-31 04:18:11 +0200

    End of Section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Helpers
                 opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
             });
             CreateMap<Models.Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, Models.User>();
         }
     }
 }
